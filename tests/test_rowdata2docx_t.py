dc1c38aba748787b3d6521ca6cfa79aba1702231
ffreemt <yucongo+fmt@gmail.com>
2023-02-27 11:24:31 +0800

    Fix test_rowdata2docx_t

--- tests/test_rowdata2docx_t.py
+++ tests/test_rowdata2docx_t.py
@@ -1,11 +1,11 @@
-"""Test rowdata2file."""
+"""Test rowdata2docx_t."""
 # pylint: disable=broad-except
 from pathlib import Path
 
 from rowdata2file.rowdata2docx_t import rowdata2docx_t
 
 
-def test_rowdata2docx():
+def test_rowdata2docx_t():
     """Test rowdata2docx_t."""
     text1 = """For complete control over terminal formatting, Rich offers a Console class. Most applications will require a single Console instance, so you may want to create one at the module level or as an attribute of your top-level object. For example, you could add a file called “console.py” to your project:"""
     text2 = """The console object handles the mechanics of generating ANSI escape sequences for color and style. It will auto-detect the capabilities of the terminal and convert colors if necessary."""
@@ -27,12 +27,15 @@ def test_rowdata2docx():
         print(exc)
         out = str(exc)
 
+    stem = infilepath.stem
+    outfilepath = infilepath.with_name(f"{stem}-t.docx")
+
     assert "Saved" in out
-    assert Path(infilepath).with_suffix(".docx").exists()
+    assert outfilepath.exists()
 
     # remove outfilepath
     try:
         ...
-        # Path(infilepath).with_suffix(".docx").unlink()
+        outfilepath.unlink()
     except Exception:
         print(f"Cant delete {Path(infilepath).with_suffix('docx')}")
